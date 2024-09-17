62a6c0d1bffa332b5ecd2880b7f7b51ca2882373
Leonardo Valente <leoovalente47@gmail.com>
2024-09-17 15:57:10 -0300

    Exercicios Módulo 7 EBAC

---
+++ md7/exercicio-classes/src/Carro.java
@@ -0,0 +1,71 @@
+public class Carro {
+    private String cor;
+
+    private String placa;
+
+    private String modelo;
+
+    private String marca;
+
+    private int ano;
+
+    private int documento;
+
+    private String dono;
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(int documento) {
+        this.documento = documento;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+}

e90f067daa5d2725809b049ab5965e05fd232428
Leonardo Valente <leoovalente47@gmail.com>
2024-09-17 21:51:31 -0300

    Tarefas Módulo 8 EBAC

--- md7/exercicio-classes/src/Main.java
+++ md7/exercicio-classes/src/Main.java
@@ -2,6 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+            int num1;
+            int num2;
+    System.out.println("Digite um número:");
+    num1 = Integer.parseInt(System.console().readLine());
+    System.out.println(num1);
         }
     }
---
+++ md8/tarefa-calculomedia/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    public static void main(String[] args) {
+       double num1 = 5.9;
+       double num2 = 5.7;
+       double num3 = 9.5;
+       double num4 = 10;
+       double media;
+
+        media = (num1 + num2 + num3 + num4) / 4;
+        System.out.println("Media: " + media);
+
+
+
+
+
+
+    }
+    }

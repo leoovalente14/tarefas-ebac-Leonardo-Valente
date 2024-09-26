99753007178f57bacde65dc770a4b9269b00c71e
Leonardo Valente <leoovalente47@gmail.com>
2024-09-26 02:49:59 -0300

    Módulo 9 EBAC

---
+++ md9/tarefa-wrapper/src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer num2;
+        int num1;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+         num1 = scanner.nextInt();
+
+         num2 = num1;
+
+         System.out.print(num2);
+
+
+        }
+    }
